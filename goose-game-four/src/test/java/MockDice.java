4d019d631c7579b2b84699d8c2c6c3bf004711dc
Marco Testa <marco.testa@intre.it>
2023-11-26 17:03:13 +0100

    test: check if MockDice method roll is called twice

--- goose-game-four/src/test/java/MockDice.java
+++ goose-game-four/src/test/java/MockDice.java
@@ -2,16 +2,23 @@ public class MockDice implements Dice {
 
     private int value;
 
+    private boolean alreadyRolled = false;
+
     public MockDice(int value) {
         this.value = value;
     }
 
     public void setValue(int value) {
         this.value = value;
+        this.alreadyRolled = false;
     }
 
     @Override
     public int roll() {
+        if (this.alreadyRolled) {
+            throw new RuntimeException("You cannot call method roll twice");
+        }
+        this.alreadyRolled = true;
         return this.value;
     }
 }
